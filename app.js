31cd827a53e0cf57278580342bc0c4b06293e042
Sadique <mhsadique1307@gmail.com>
2021-02-08 13:48:42 +0600

    Till now the food items can only be searched by letter like 'a' or 'g'

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+
+function addItems() {
+    // let container = document.getElementById('container');
+    let inputValue = document.getElementById('search-food').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${inputValue}`)
+    .then(res => res.json())
+    .then(data => {
+        data.meals.forEach(item => {
+            console.log(item.strMeal);
+            let parentDiv = document.createElement('div');
+            parentDiv.className = 'container';
+            document.body.append(parentDiv);
+            let div = document.createElement('div');
+            div.className = 'individual';
+            div.innerText = item.strMeal;
+            parentDiv.append(div);
+        });
+        // console.log(data.meals.length)
+
+
+        // console.log(data.length)
+        // let div = document.createElement('div');
+        // div.className = 'individual';
+        // div.innerText = data[item][i].strMeal;
+        // container.append(div);
+    });
+}
+
